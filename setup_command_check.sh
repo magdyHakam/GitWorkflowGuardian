eba4de52e6110baf1227899664dd07a0eedd3b85
Magdy.Hakam <magdy.hakam@gmail.com>
2024-01-08 04:19:25 +0200

    different approach

--- setup_command_check.sh
+++ setup_command_check.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# Define the function to add to .bashrc or .bash_profile
-read -r -d '' COMMAND_CHECK_FUNCTION << 'EOF'
-function check_git_command() {
-    if [[ $1 == git\ merge\ dev* ]]; then
-        current_branch=\$(git rev-parse --abbrev-ref HEAD)
-        if [[ \$current_branch != dev-* ]]; then
-            echo "Warning: You are not on a 'dev-' branch."
-            return 1
-        fi
-    elif [[ $1 == git\ merge\ stg* ]]; then
-        current_branch=\$(git rev-parse --abbrev-ref HEAD)
-        if [[ \$current_branch != stg-* ]]; then
-            echo "Warning: You are not on a 'stg-' branch."
-            return 1
-        fi
+# Function definition to be added to .bashrc or .bash_profile
+COMMAND_CHECK_FUNCTION=$(cat << 'EOF'
+###########
+git_merge_check() {
+    local branch_prefix=${1:0:3}
+    local current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+    if [[ $current_branch != $branch_prefix-* ]]; then
+        echo "Warning: git merge $branch_prefix can only be run from a branch starting with '$branch_prefix-'."
+    else
+        command git merge "$@"
     fi
-    eval \$@
 }
-trap 'check_git_command \$BASH_COMMAND' DEBUG
+
+git() {
+    if [[ $1 == "merge" && ($2 == "dev" || $2 == "stg") ]]; then
+        git_merge_check "$2"
+    else
+        command git "$@"
+    fi
+}
+
 EOF
+)
 
 # Append the function to .bashrc or .bash_profile
 echo "$COMMAND_CHECK_FUNCTION" >> ~/.bashrc
